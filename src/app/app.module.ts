c39f2892da62a432e51aeed18a950e955c0f2d6d
Luigi <luigitosin@gmail.com>
2020-05-30 13:28:46 -0300

    Menu e routing de paginas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,45 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon'; 
+import {RouterModule} from '@angular/router'
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BarraNavegacaoComponent } from './barra-navegacao/barra-navegacao.component';
+import { ClienteComponent } from './cliente/cliente.component';
+import { ProdutoComponent } from './produto/produto.component';
+import { PedidoComponent } from './pedido/pedido.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BarraNavegacaoComponent,
+    ClienteComponent,
+    ProdutoComponent,
+    PedidoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    RouterModule.forRoot( [
+      {
+        path: 'cliente',
+        component: ClienteComponent
+      },
+      {
+        path: 'produto',
+        component: ProdutoComponent
+      },
+      {
+        path: 'pedido',
+        component: PedidoComponent
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
